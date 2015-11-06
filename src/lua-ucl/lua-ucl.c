87d80b8ea2cf8554f4a860d43484f9c22f45e7fb
Boris Nagaev <bnagaev@gmail.com>
2015-11-07 02:55:46 +0300

    implement basic (de)compression

--- src/lua-ucl/lua-ucl.c
+++ src/lua-ucl/lua-ucl.c
@@ -20,11 +20,53 @@
 #endif
 
 int luaucl_compress(lua_State* L) {
-    return 0;
+    size_t input_len;
+    const char* input = luaL_checklstring(L, 1, &input_len);
+    // see UCL's README
+    ucl_uint output_len = input_len + (input_len / 8) + 256;
+    void* output = lua_newuserdata(L, output_len);
+    ucl_progress_callback_p callback = NULL;
+    int level = 9;
+    struct ucl_compress_config_p conf = NULL;
+    ucl_uintp result = NULL;
+    int status = ucl_nrv2b_99_compress(
+        input,
+        input_len,
+        output,
+        &output_len,
+        callback,
+        level,
+        conf,
+        result
+    );
+    if (status != UCL_E_OK) {
+        // TODO implement all statuses from uclconf.h
+        return luaL_error(L, "UCL error occurred");
+    }
+    lua_pushlstring(L, output, output_len);
+    return 1;
 }
 
 int luaucl_decompress(lua_State* L) {
-    return 0;
+    size_t input_len;
+    const char* input = luaL_checklstring(L, 1, &input_len);
+    // output_len is max possible output
+    int output_len = luaL_checkinteger(L, 2);
+    void* output = lua_newuserdata(L, output_len);
+    ucl_voidp wrkmem = NULL;
+    int status = ucl_nrv2b_decompress_8(
+        input,
+        input_len,
+        output,
+        &output_len,
+        wrkmem
+    );
+    if (status != UCL_E_OK) {
+        // TODO implement all statuses from uclconf.h
+        return luaL_error(L, "UCL error occurred");
+    }
+    lua_pushlstring(L, output, output_len);
+    return 1;
 }
 
 luaL_Reg luaucl_functions[] = {
